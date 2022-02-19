c25f1a8cb7e1e8c0b2e6986e65fd4ae728a4e40c
OldZealand03 <isaacwallacesmith@gmail.com>
2022-02-19 12:04:30 -0700

    hello

--- practice/javascript/practice.js
+++ practice/javascript/practice.js
@@ -47,11 +47,11 @@ function isEven(number) {
     else if (number == 1) {
         return false;
     }
-    else if (number < 0 ) {
+    else if (number < 0) {
         return isEven(number * -1);
     }
     else {
-        return isEven(number-2);
+        return isEven(number - 2);
     }
 }
 
@@ -60,12 +60,12 @@ function isEven(number) {
 function range(start, end, step = 1) {
     array = [];
     if (step < 0) {
-        for (i = start; i > end-1; i+=step){
+        for (i = start; i > end - 1; i += step) {
             array.push(i);
         }
     }
     else {
-        for (i = start; i < end+1; i+=step){
+        for (i = start; i < end + 1; i += step) {
             array.push(i);
         }
     }
@@ -91,7 +91,7 @@ function arrayToList(array) {
         return list;
     } else {
         list.value = array[0];
-        array.splice(0,1);
+        array.splice(0, 1);
 
         list.rest = arrayToList(array);
         return list;
@@ -115,7 +115,7 @@ function prepend(val, res) {
 function nth(list, n) {
     var num;
     count = 0;
-    while (count != n+1) {
+    while (count != n + 1) {
         num = list.value;
         list = list.rest;
         count++;
@@ -126,7 +126,7 @@ function nth(list, n) {
 // Chapter 5
 
 let arrays = [[1, 2, 3], [4, 5], [6]];
-console.log(arrays.reduce((finalArray, index) => {finalArray.concat(index);}, 0));
+console.log(arrays.reduce((finalArray, index) => { finalArray.concat(index); }, 0));
 
 
 function every(array, test) {
@@ -136,11 +136,11 @@ function every(array, test) {
     });
     return !passed;
 }
-  
-  
+
+
 console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
   // → true
