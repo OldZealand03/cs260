757129dfec043f96120af06d62cb8a3ff2ef9918
OldZealand03 <isaacwallacesmith@gmail.com>
2022-02-25 21:27:35 -0700

    v2

--- practice/vue/v1/script.js
+++ practice/vue/v1/script.js
@@ -40,44 +40,68 @@
 
 
 // Activity 4
+// let app = new Vue({
+//   // bind it to the #root div in the DOM
+//   el: "#root",
+//   // provide data for bindings
+//   data: {
+//     message: 'I really like bread',
+//     tasks: [{
+//         description: "Create some loaves of bread",
+//         completed: true
+//       },
+//       {
+//         description: "Create some fish",
+//         completed: true
+//       },
+//       {
+//         description: "Create more loaves of bread",
+//         completed: false,
+//       },
+//       {
+//         description: "Create more fish",
+//         completed: false
+//       },
+//       {
+//         description: "Create more loaves of bread",
+//         completed: false
+//       },
+//       {
+//         description: "Tell Peter to clean up the mess",
+//         completed: false
+//       }
+//     ]
+//   },
+//   computed: {
+//     screaming() {
+//       return this.message.toUpperCase();
+//     },
+//     incompleteTasks() {
+//       return this.tasks.filter(task => !task.completed);
+//     }
+//   }
+// });
+
+
 let app = new Vue({
-  // bind it to the #root div in the DOM
-  el: "#root",
-  // provide data for bindings
+  el: '#root',
   data: {
-    message: 'I really like bread',
-    tasks: [{
-        description: "Create some loaves of bread",
-        completed: true
-      },
-      {
-        description: "Create some fish",
-        completed: true
-      },
-      {
-        description: "Create more loaves of bread",
+      todos: [{
+        text: "make an app",
         completed: false,
-      },
-      {
-        description: "Create more fish",
+      }, {
+        text: "declare victory",
+        completed: false,
+      }, {
+        text: "profit",
         completed: false
-      },
-      {
-        description: "Create more loaves of bread",
-        completed: false
-      },
-      {
-        description: "Tell Peter to clean up the mess",
-        completed: false
-      }
-    ]
-  },
-  computed: {
-    screaming() {
-      return this.message.toUpperCase();
+      }],
+      message: '',
     },
-    incompleteTasks() {
-      return this.tasks.filter(task => !task.completed);
+    methods: {
+      addItem() {
+        this.todos.push({text: this.message, completed:false});
+        this.message = '';
+      },
     }
-  }
 });
